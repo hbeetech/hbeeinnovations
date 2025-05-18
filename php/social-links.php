8bec4111533ffa8a903c9bf563ca1d112f40b04a
hbeetech <hbeetechie@gmail.com>
2025-05-18 18:17:32 +0530

    Delete social-links.php

--- php/social-links.php
+++
@@ -1,5 +0,0 @@
-<a class="btn-square text-primary border-end rounded-0"
-   href="https://www.facebook.com/profile.php?id=61569982450220"
-   rel="noopener noreferrer">
-   <i class="fab fa-facebook-f"></i>
-</a>
